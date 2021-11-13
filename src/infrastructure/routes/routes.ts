6dfd46627af1ffd97f48b5a0e2d560b946b14eb1
Luiz Mendes <luiz.mendes@in8.com.br>
2021-11-13 18:19:13 -0300

    refactor: ident code

--- src/infrastructure/routes/routes.ts
+++ src/infrastructure/routes/routes.ts
@@ -3,12 +3,12 @@ import { Router, Response, Request } from 'express';
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-    res.status(200).send('ok');
+  res.status(200).send('ok');
 });
 
 router.post('/', (req: Request, res: Response) => {
-    console.log(req.body);
-    res.send('ok');
+  console.log(req.body);
+  res.send('ok');
 });
 
 export default router;
